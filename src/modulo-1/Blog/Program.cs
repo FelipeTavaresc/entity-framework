ee8c809545369d1040417b0116de679196204881
Felipe Tavares <felipe.tavares@outlook.com>
2024-06-26 08:20:29 -0300

    Explicação do usu do ToList()

--- src/modulo-1/Blog/Program.cs
+++ src/modulo-1/Blog/Program.cs
@@ -22,10 +22,28 @@ namespace Blog
                 // context.Update(tag);
                 // context.SaveChanges();
 
-                var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+                // var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
 
-                context.Remove(tag);
-                context.SaveChanges();
+                // context.Remove(tag);
+                // context.SaveChanges();
+
+                //Exemplo incorreto, filtrando a lista em memória
+                // var tags = context
+                //             .Tags
+                //             .ToList()
+                //             .Where(x => x.Name.Contains(".NET"));
+
+                //Exemplo correto, filtrando a lista e só por último executando a query no banco
+                var tags = context
+                            .Tags
+                            .Where(x => x.Name.Contains(".NET"))
+                            .ToList();
+                            
+
+                foreach (var tag in tags)
+                {
+                    Console.WriteLine(tag.Name);
+                }
             }
         }
     }
