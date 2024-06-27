487fa2ba3a06a04e52a0dd1609462b74fbca428c
Felipe Tavares <felipe.tavares@outlook.com>
2024-06-27 05:49:15 -0300

    First e Single

--- src/modulo-1/Blog/Program.cs
+++ src/modulo-1/Blog/Program.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using Blog.Data;
 using Blog.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Blog
 {
@@ -34,16 +35,54 @@ namespace Blog
                 //             .Where(x => x.Name.Contains(".NET"));
 
                 //Exemplo correto, filtrando a lista e só por último executando a query no banco
-                var tags = context
-                            .Tags
-                            .Where(x => x.Name.Contains(".NET"))
-                            .ToList();
+                // var tags = context
+                //             .Tags
+                //             .Where(x => x.Name.Contains(".NET"))
+                //             .ToList();
                             
+                // var tags = context
+                //             .Tags
+                //             .ToList();
 
-                foreach (var tag in tags)
-                {
-                    Console.WriteLine(tag.Name);
-                }
+                // foreach (var tag in tags)
+                // {
+                //     Console.WriteLine(tag.Name);
+                // }
+
+
+                //Exemplo incorreto, não utilizar AsNoTracking em update ou delete
+                // var tag = context
+                //             .Tags
+                //             .AsNoTracking()
+                //             .FirstOrDefault(x => x.Id == 4);
+                // tag.Name = "Ponto NET1";
+                // tag.Slug = "dotnet";
+
+                //Exemplo correto
+                // var tag = context
+                //             .Tags
+                //             .FirstOrDefault(x => x.Id == 4);
+                // tag.Name = "Ponto NET1";
+                // tag.Slug = "dotnet";
+
+                //Para select, ok usar AsNoTracking
+
+                // context.Update(tag);
+                // context.SaveChanges();
+
+                // Retorna o primeiro tem ou null
+                // var tag = context
+                //             .Tags
+                //             .AsNoTracking()
+                //             .FirstOrDefault(x => x.Id == 1);
+
+                //Caso tenha mais de um item para o filtro, retorna erro.
+                var tag = context
+                            .Tags
+                            .AsNoTracking()
+                            .Single(x => x.Id == 3 || x.Id == 4);
+
+                Console.WriteLine(tag?.Name);
             }
         }
     }
