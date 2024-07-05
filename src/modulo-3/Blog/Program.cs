50d2048c18948573cbf3a3b1714d450db216b169
Felipe Tavares <felipe.tavares@outlook.com>
2024-07-05 06:17:49 -0300

    Iniciando módulo 3

---
+++ src/modulo-3/Blog/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+using Blog.Data;
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var context = new BlogDataContext();
+            
+        }
+    }
+}
