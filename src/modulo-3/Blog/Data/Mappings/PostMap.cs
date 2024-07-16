e29a0cae597eca0f82d03cedafa3c06937a6aba2
Felipe Tavares <felipe.tavares@outlook.com>
2024-07-16 10:54:56 -0300

    Relacionamento N:N

--- src/modulo-3/Blog/Data/Mappings/PostMap.cs
+++ src/modulo-3/Blog/Data/Mappings/PostMap.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Blog.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -42,6 +43,22 @@ namespace Blog.Data.Mappgins
                 .WithMany(x => x.Posts)
                 .HasConstraintName("FK_Post_Category")
                 .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(x => x.Tags)
+                .WithMany(x => x.Posts)
+                .UsingEntity<Dictionary<string, object>>(
+                    "PostTag",
+                    post => post.HasOne<Tag>()
+                            .WithMany()
+                            .HasForeignKey("PostId")
+                            .HasConstraintName("FK_PostTag_PostId")
+                            .OnDelete(DeleteBehavior.Cascade),
+                    tag => tag.HasOne<Post>()
+                            .WithMany()
+                            .HasForeignKey("TagId")
+                            .HasConstraintName("FK_PostTag_TagId")
+                            .OnDelete(DeleteBehavior.Cascade)
+                );
         }
     }
 }
