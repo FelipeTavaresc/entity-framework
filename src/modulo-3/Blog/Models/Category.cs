017ca7bc9234afc4ed828272d96f0523df970632
Felipe Tavares <felipe.tavares@outlook.com>
2024-07-16 10:43:56 -0300

    Relacionamentos 1:N

--- src/modulo-3/Blog/Models/Category.cs
+++ src/modulo-3/Blog/Models/Category.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -10,5 +11,7 @@ namespace Blog.Models
         public string Name { get; set; }
 
         public string Slug { get; set; }
+
+        public List<Post> Posts { get; set; }
     }
 }
