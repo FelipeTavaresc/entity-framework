f8bca45769b1a2197140ea8618cb17bdf22d1eed
Felipe Tavares <felipe.tavares@outlook.com>
2024-07-18 18:08:44 -0300

    Testando os relacionamentos

--- src/modulo-3/Blog/Program.cs
+++ src/modulo-3/Blog/Program.cs
@@ -12,6 +12,33 @@ namespace Blog
         {
             using var context = new BlogDataContext();
             
+            // context.Users.Add(new User{
+            //     Bio = "...",
+            //     Email = "email@email",
+            //     Image = "https://image",
+            //     Name = "User",
+            //     PasswordHash = "!#454",
+            //     Slug = "user"
+            // });
+
+            // context.SaveChanges();
+
+            var user = context.Users.FirstOrDefault();
+            var post = new Post{
+                Author = user,
+                Body = "Meu artigo",
+                Category = new Category{
+                    Name = "Backend",
+                    Slug = "backend"
+                },
+                CreateDate = DateTime.Now,
+                Slug = "meu-artigo",
+                Summary = "Resumo artigo",
+                Title = "Meu Artigo"
+            };
+
+            context.Posts.Add(post);
+            context.SaveChanges();
         }
     }
 }
